ef5f06b19deaffb3f7a28f02222a26fa0a3c60a4
mohamed essm <langara5000@hotmail.com>
2024-04-04 17:52:01 +0200

    init

--- lib/shared/components/constants.dart
+++ lib/shared/components/constants.dart
@@ -1,4 +1,4 @@
 //var baseUrl = 'https://10.0.2.2:7082'; //Local Host
-var baseUrl = 'https://d09b-156-208-202-44.ngrok-free.app'; //----ngrok
+var baseUrl = 'https://d09b-156-208-202-44.ngrok-free.app'; //-----ngrok
 //var baseUrl = 'https://10.0.2.2:7082';//Paymob Integration
 var webUrl = 'http://10.0.2.2:808';
